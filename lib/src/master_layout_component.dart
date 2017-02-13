07a25fdc5c11a96c35c5907d1315624f0fbfb2e2
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-02-13 14:16:28 +0300

    -

--- lib/src/master_layout_component.dart
+++ lib/src/master_layout_component.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
@@ -5,4 +6,13 @@ import 'package:angular2/router.dart';
 @View(
     templateUrl: 'master_layout_component.html',
     directives: const [RouterLink])
-class MasterLayoutComponent {}
+class MasterLayoutComponent implements AfterViewInit {
+  @override
+  void ngAfterViewInit() {
+    var script = new ScriptElement()
+      ..async = true
+      ..type = 'text/javascript'
+      ..src = 'packages/master_layout/src/assets/js/app.js';
+    document.body.append(script);
+  }
+}
