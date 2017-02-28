db335644166f562f44189848e24d024b0a002375
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-28 14:54:30 +0300

    CMAS-288 Переделать систему маршрутизации на фронтенде

--- lib/src/breadcrumb/breadcrumb_component.dart
+++ lib/src/breadcrumb/breadcrumb_component.dart
@@ -1,31 +1,73 @@
-import 'dart:html';
-
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
+import "package:angular2/platform/common.dart" show Location;
+
+class BreadcrumbData {
+  String displayName;
+  String url;
+}
 
 @Component(selector: 'breadcrumb')
 @View(templateUrl: 'breadcrumb_component.html')
-class BreadcrumbComponent implements AfterViewInit {
+class BreadcrumbComponent
+    implements AfterViewInit {
   final Router _router;
+  final ChangeDetectorRef _changeDetectorRef;
+  Location _location;
 
-  BreadcrumbComponent(this._router) {}
+  Map<Type, BreadcrumbData> items = new Map<Type, BreadcrumbData>();
+
+  BreadcrumbComponent(this._changeDetectorRef, this._router, this._location) {}
 
   @override
   void ngAfterViewInit() {
-/*
-    print('BreadcrumbComponent init');
+    _buildBreadcrumbs(this._router.currentInstruction);
 
-    print('root: ${this._router.root}');
-    print('host: ${this._router.hostComponent}');
-    print('parent: ${this._router.parent}');
-
-    this._router.root.subscribe((e){
-
-      print('onNext: $e');
-      print('root: ${this._router.root}');
-      print('host: ${this._router.hostComponent}');
-      print('parent: ${this._router.parent}');
-
-    });*/
+    this._router.root.subscribe((e) {
+      this._router.recognize(e).then((i) {
+        _buildBreadcrumbs(i);
+      });
+    });
   }
+
+  void _buildBreadcrumbs(Instruction instruction) {
+    if (instruction == null)
+      return;
+
+    items.clear();
+
+    var current_instruction = instruction;
+
+    items[current_instruction.component.componentType] =
+        getData(current_instruction);
+
+    while (current_instruction.child != null) {
+      current_instruction = current_instruction.child;
+
+      items[current_instruction.component.componentType] =
+          getData(current_instruction);
+    }
+
+    _changeDetectorRef.detectChanges();
+  }
+
+  BreadcrumbData getData(Instruction instruction) {
+    var result = new BreadcrumbData();
+
+    /*
+    * TODO: Формирование ссылки
+    * result.url = this._location.prepareExternalUrl(_router.generate([instruction.component.routeName]).toLinkUrl());
+    * */
+    result.url = '#';
+
+    if (instruction.component.routeData != null) {
+      result.displayName = instruction.component.routeData.data['displayName'];
+    }
+
+    if (result.displayName == null)
+      result.displayName = instruction.component.routeName;
+
+    return result;
+  }
+
 }
