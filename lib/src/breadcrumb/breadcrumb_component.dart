f226485a8559bb91a4385bcc583535f3fe44f92b
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-03-07 09:35:21 +0300

    fix location

--- lib/src/breadcrumb/breadcrumb_component.dart
+++ lib/src/breadcrumb/breadcrumb_component.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-import 'package:angular2/platform/common.dart';
+import 'package:angular2/platform/common.dart' show Location;
 
 class BreadcrumbData {
   String displayName;
