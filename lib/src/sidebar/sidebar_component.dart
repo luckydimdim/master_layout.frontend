d5c37b61ab70297a833c34e5a16f1831e188ee8c
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-29 11:27:04 +0300

    admin link added

--- lib/src/sidebar/sidebar_component.dart
+++ lib/src/sidebar/sidebar_component.dart
@@ -2,12 +2,21 @@ import 'dart:html';
 
 import 'package:angular2/core.dart';
 import 'package:angular_utils/directives.dart';
+import 'package:auth/auth_service.dart';
 
 @Component(
     selector: 'sidebar',
     templateUrl: 'sidebar_component.html',
     directives: const [CmRouterLink])
 class SidebarComponent implements AfterViewInit {
+
+  final AuthorizationService _authorizationService;
+
+  SidebarComponent(this._authorizationService) {
+    isAdmin = _authorizationService.isInRole(Role.Administrator);
+  }
+
+  bool isAdmin = false;
 
   @override
   void ngAfterViewInit() {
